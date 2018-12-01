d12de2f070a6ccfa8a57096aab6551da2521f94d
bucko13 <bucko.perley@gmail.com>
2018-11-30 17:55:51 -0800

    fixes

--- src/components/Paper.js
+++ src/components/Paper.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 import { connectTheme } from '../utils';
+import { Header } from '.';
 
-function Paper({ theme, style, customClassNames, header, children, type }) {
-  let classNames = `${customClassNames ? `${customClassNames} ` : ''} ${theme
-    .paper.default}`;
-  if (type === 'error') classNames = `${classNames} ${theme.paper.error}`;
-  return (
-    <div className={classNames} style={style}>
-      {header.length ? <Text type="h6">{header}</Text> : ''}
-      {children}
-    </div>
-  );
+class Paper extends PureComponent {
+  static get propTypes() {
+    return {
+      sytle: PropTypes.object,
+      type: PropTypes.oneOf(['error']),
+      theme: PropTypes.object,
+      customClassNames: PropTypes.string,
+      header: PropTypes.string,
+      children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node,
+        PropTypes.string,
+      ]),
+    };
+  }
+
+  render() {
+    const {
+      customClassNames,
+      theme,
+      header,
+      children,
+      type,
+      style,
+    } = this.props;
+    let classNames = `${customClassNames ? `${customClassNames} ` : ''} ${theme
+      .paper.default}`;
+    if (type === 'error') classNames = `${classNames} ${theme.paper.error}`;
+    return (
+      <div className={classNames} style={style}>
+        {header ? <Header type="h6">{header}</Header> : ''}
+        {children}
+      </div>
+    );
+  }
 }
-
-Paper.propTypes = {
-  sytle: PropTypes.object,
-  type: PropTypes.oneOf(['error']),
-  customClassNames: PropTypes.string,
-  header: PropTypes.string,
-  children: React.PropTypes.element,
-};
 
 export default connectTheme(Paper);
--- src/components/index.js
+++ src/components/index.js
@@ -11,6 +11,7 @@ import Text from './Text';
 import QRCode from './QRCode';
 export { default as Spinner } from './Spinner';
 export { default as Label } from './Label';
+export { default as Paper } from './Paper';
 
 export {
   Button,
